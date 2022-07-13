157e4fb0cd219bfe40635c0f433c70564bbd290f
sat221 <84304938+sat221@users.noreply.github.com>
2022-07-13 10:23:19 +0530

    Add files via upload

---
+++ Swap.java
@@ -0,0 +1,16 @@
+class Swap{
+public static void main(String[] args){
+int a=10;
+int b=16;
+int c=0	;
+ System.out.println("before swapping.."+a);
+System.out.println("before swapping.."+b);
+
+c=a;
+a=b;
+b=c;
+System.out.println("after swapping.."+a);
+System.out.println("before swapping.."+b);
+
+}
+}
